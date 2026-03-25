fc871138e70fd621506e324bd4e434dc463a2ae0
Vitali Romanski <vitromansky@gmail.com.>
2026-03-25 19:14:00 +0100

    No gui for Jenkins now

--- src/test/java/com/carbooking/gui/core/TestBase.java
+++ src/test/java/com/carbooking/gui/core/TestBase.java
@@ -1,5 +1,6 @@
 package com.carbooking.gui.core;
 
+//import com.carbooking.gui.fw.ApplicationManager;
 import com.carbooking.gui.fw.ApplicationManager;
 import com.carbooking.gui.pages.BookingPage;
 import com.carbooking.gui.pages.HomePage;
@@ -10,26 +11,27 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
-    protected final ApplicationManager app = new ApplicationManager("chrome");
+ //   protected final ApplicationManager app = new ApplicationManager("chrome");
     protected HomePage homePage;
     protected LoginPage loginPage;
     protected RegistrationPage registrationPage;
     protected BookingPage bookingPage;
 
-    @BeforeMethod
-    public void setUp() {
-        app.init();
-        homePage = new HomePage(app.driver);
-        loginPage = new LoginPage(app.driver);
-        registrationPage = new RegistrationPage(app.driver);
-        bookingPage = new BookingPage(app.driver);
-    }
+//
+//    @BeforeMethod
+//    public void setUp() {
+//        app.init();
+//        homePage = new HomePage(app.driver);
+//        loginPage = new LoginPage(app.driver);
+//        registrationPage = new RegistrationPage(app.driver);
+//        bookingPage = new BookingPage(app.driver);
+//    }
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown(ITestResult result) {
-        if (!result.isSuccess()) {
-            app.takeScreenshot();
-        }
-        app.stop();
-    }
+//    @AfterMethod(alwaysRun = true)
+//    public void tearDown(ITestResult result) {
+//        if (!result.isSuccess()) {
+//            app.takeScreenshot();
+//        }
+//        app.stop();
+//    }
 }
