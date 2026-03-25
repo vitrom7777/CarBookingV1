47083378bd65ea41bc94bcc7b74c86b5524cdf1d
Vitali Romanski <vitromansky@gmail.com.>
2026-03-25 11:00:40 +0100

    Add AllTestRunner.java in GUI

---
+++ src/test/java/com/carbooking/gui/tests/all/AllTestsRunner.java
@@ -0,0 +1,29 @@
+package com.carbooking.gui.tests.all;
+
+import com.carbooking.gui.tests.SecurityTests;
+import com.carbooking.gui.tests.negative.BookingNegativeTests;
+import com.carbooking.gui.tests.negative.LoginNegativeTests;
+import com.carbooking.gui.tests.positive.BookingPositiveTests;
+import com.carbooking.gui.tests.positive.LoginPositiveTests;
+import com.carbooking.gui.tests.positive.RegistrationPositiveTests;
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+
+public class AllTestsRunner {
+
+    @Test
+    public void runEverything() {
+        TestNG testng = new TestNG();
+       
+        testng.setTestClasses(new Class[] {
+                LoginPositiveTests.class,
+                BookingPositiveTests.class,
+                RegistrationPositiveTests.class,
+                LoginNegativeTests.class,
+                BookingNegativeTests.class,
+                SecurityTests.class, // Добавили новый класс в запуск
+                FullWorkflowTest.class
+        });
+        testng.run();
+    }
+}
